0ba2909a112caa349f0b5439b15d6a0c5dd0014c
Michał Szymecki <szymsonik@gmail.com>
2020-01-24 00:23:05 +0100

    create program

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    boolean isEven(int a) {
+        return a % 2 == 0;
+    }
+
+    boolean isOdd(int a) {
+        return a % 2 != 0;
+    }
+
+    double circleField(double r) {
+        return 3.14*r*r;
+    }
+
+    double power(int a) {
+        return a * a;
+    }
+
+}
