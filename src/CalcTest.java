ee396147ad258f7d5e004f9275f44035f9415def
Michał Szymecki <szymsonik@gmail.com>
2020-01-25 00:02:35 +0100

    correct some mistakes

--- src/CalcTest.java
+++ src/CalcTest.java
@@ -1,11 +1,11 @@
 public class CalcTest {
     public static void main(String[] args) {
-        int k = 7;
+        int k = 6;
         double z = 8;
         Calculator calculator = new Calculator();
-        System.out.println("Is the number Even: " + calculator.isEven(k));
-        System.out.println("Is the number Odd: " + calculator.isOdd(k));
-        System.out.println("Circle field with a radius of a " + z + " = " + calculator.circleField(z));
+        System.out.println("Is the number " + k + " oven: " + calculator.isEven(k));
+        System.out.println("Is the number " + k + " Odd: " + calculator.isOdd(k));
+        System.out.println("Circle field with a radius of " + z + " = " + calculator.circleField(z));
         System.out.println(k + " to the power of 2 = "+calculator.power(k));
     }
 
